398f1a8aa695d2a4835b70448b0a83e8b7eaaa58
Nicole George <hello@nicolegeorge.com.au>
2023-02-10 13:21:41 +1030

    sidebar and main area components done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,21 @@
-import React from "react"
+import React from 'react';
 
-function Sidebar() {
+function Sidebar({ size, color, setSize, setColor }) {
   return (
-    <div className="sidebar">
-      <input type="text" />
-      <input type="text" />
+    <div className='sidebar'>
+      <input
+        type='text'
+        value={size}
+        onChange={(e) => setSize(e.target.value)}
+      />
+      <input
+        type='text'
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
       <button>Make the text 20px and pink</button>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
